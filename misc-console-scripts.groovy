06d4153ae045fcb26c31295c17603ef24f55825f
akomakom <akomakom@users.noreply.github.com>
2016-03-03 10:20:33 -0500

    plugin diffing

--- misc-console-scripts.groovy
+++ misc-console-scripts.groovy
@@ -66,6 +66,15 @@ selectFrom.findAll{job -> job instanceof AbstractProject && !job.isDisabled() &&
 count 
  
  
+/** 
+ * List all installed plugins so the text output can be diffed with another Jenkins 
+ **/
+Jenkins.instance.pluginManager.plugins.sort().each{
+  plugin -> 
+    println ("${plugin.getDisplayName()} (${plugin.getShortName()}): ${plugin.getVersion()}")
+}
+Jenkins.instance.pluginManager.plugins.size()
+ 
  
 
 
