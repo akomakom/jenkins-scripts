45d587a494b697081b47a8f0b5a14e679078a87d
akomakom <akomakom@users.noreply.github.com>
2016-09-28 13:22:18 -0400

    Showing build count

--- misc-console-scripts.groovy
+++ misc-console-scripts.groovy
@@ -2,35 +2,35 @@
  * Find jobs that do not discard old builds or keep too many.
  * Optionally configure them to discard builds (LogRotator/BuildDiscarder)
  */
-import groovy.xml.*;
-import hudson.model.*;
-import hudson.maven.*;
-import jenkins.model.*;
-import jenkins.maven.*;
-import hudson.*;
-  
 def count = 0
 def buildWarning = 30 //anything over 30 is considered suspicious
 
-Jenkins.instance.items.findAll{job ->  job instanceof Job && !job.isDisabled() }.each{
+Jenkins.instance.items.findAll{job ->  job instanceof Job }.each{
 job ->
   count++
     //println("\nBegin Processing ${job.name} (${job.class}")
   def rotator = job.buildDiscarder
+  def problem = false
   
   if (rotator == null) {
     println "${job.fullName}: Not configured!"
-    
+    problem = true
     //set a new build discarder (uncomment to make changes)
     //job.setBuildDiscarder(new hudson.tasks.LogRotator(-1, 7, -1, -1))
     //println "Added one..."
   } else {
     if ( (rotator.numToKeep <= 0 && rotator.daysToKeep <= 0) || rotator.numToKeep > buildWarning || rotator.daysToKeep > buildWarning) {
       println "${job.fullName}: Problematic: Num: ${rotator.numToKeep} / Days: ${rotator.daysToKeep}"
+      problem = true
     }
+  }
+  
+  if (problem) {
+    println "Total builds: ${job.getBuilds().size()}"
   }
 }
 count
+
 
 
 
